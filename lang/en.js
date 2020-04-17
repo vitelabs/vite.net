746e830b7261450e19f135e1d3e743254f7f2c61
allen <allen@vite.org>
2020-04-17 22:50:29 +0800

    version English

--- lang/en.js
+++ lang/en.js
@@ -1,5 +1,74 @@
 export default {
+  title: 'Vite - World-First DAG Based Blockchain with Smart Contract Capability',
+  meta: {
+    description: 'World-First DAG Based Blockchain with Smart Contract Capability'
+  },
+  nav: {
+    vitex: 'ViteX Exchange',
+    app: 'Vite App',
+    vitePlus: 'Vite Plus',
+    explorer: 'Block Explorer',
+    webWallet: 'Web Wallet',
+    forum: 'Forum'
+  },
+  slogan: 'World-First DAG Based Blockchain with Smart Contract',
   vitex: {
-    title: 'Exchange By the Community, For the Community'
+    title: 'A Truly Decentralized Exchange By the Community, For the Community',
+    description: `
+        <p>ViteX is the world’s first DAG based decentralized exchange.</p>
+        <p>Reinforcing decentralized governance and to protect personal data, ViteX requires NO KYC and supports self-assisted project listings so that anyone can trade on the exchange without concerning about exposing personal information.</p>
+        <p>Users that trade, market-make, stake, invite friends, or list projects on ViteX can mine platform coin VX. ViteX will return to VX holders 100% of platform collected trading fees in the form of dividends.</p>
+        <p>With the secure cross-chain enablement, any crypto asset can be traded on ViteX. Fast on-chain order matching and instant execution of trades on ViteX will bring you the trading experience just as using a centralized exchange, only safer because your assets are kept to yourself.</p>
+    `,
+    useNow: 'Try ViteX',
+    learnMore: 'Learn More'
+  },
+  app: {
+    title: 'Vite Multi-Chain HD Wallet App',
+    description: `
+        <p>An open-source multi-currency wallet that supports crypto assets of different chains.</p>
+        <p>Secure and easy management of assets with only one set of mnemonic phrases.</p>
+        <p>In-App Exchange allows for convenient trading and mining.</p>
+        <p>Loaded with DApps, get entertained with investing.</p>
+        <p>Recommended by leading crypto mining and staking pools such as BTC.com, Sparkpool, F2Pool, Luxor, and more.</p>
+    `,
+    ios: 'Download iOS',
+    android: 'Download Android',
+    learnMore: 'Learn More'
+  },
+  vitePlus: {
+    title: 'Vite Plus Donation Platform',
+    description: `
+        <p>A traceability platform powered by the VitePlus technology to address the issue of tracking donations for fighting COVID-19 global epidemic.</p>
+        <p>Grant visibility to aggregated, verified and tamper-proof data of donated funds and supplies, all data stored on a VitePlus-enabled blockchain.</p>
+        <p>Track various information, including, but not limited to, historical data and quantity of donations, the pipeline and location of donated products during transportation.</p>
+        <p>Assure accountability, transparency, and anti-manipulation of existing data.</p>
+    `,
+    learnMore: 'Learn More'
+  },
+  explorer: {
+    title: 'Block Explorer',
+    description: `
+        <p>Look up in seconds information such as wallet address, transaction hash, block height, and historical transaction list.</p>
+        <p>Synchronize Vite node information worldwide and enable instant and accurate data capture across the Vite network.</p>
+    `,
+    official: 'Vite Explorer',
+    viteScan: 'Vite Scan'
+  },
+  webWallet: {
+    title: 'Web Wallet',
+    description: `
+        <p>An open-source, browser-based multi-currency wallet that secures easy management and trading of multiple crypto assets.</p>
+        <p>Integrated with Exchange trading and mining capabilities, full-featured and convenient to use.</p>
+        <p>Support Ledger Nano S/X, ideal for desktop.</p>
+    `,
+    loginWebWallet: 'Wallet Login'
+  },
+  forum: {
+    title: 'Vite Forum',
+    description: `
+        <p>Our world-connect community platform that allows users to learn the latest news and updates about Vite</p>
+    `,
+    toForum: 'Visit Forum'
   }
 }
